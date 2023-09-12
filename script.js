9be63da861c988511c50d18bf0a45014f6feba2f
leiakc <57116108+leiakc@users.noreply.github.com>
2023-09-11 20:43:20 -0400

    Corrected URLs

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
 // window.onload = function() {
 
 // };
-
- var btcLink = document.getElementById("btcLink");
-
- btcLink.addEventListener("click", function() {
-     // Navigate to the target URL when clicked
-     window.location.href = "https:/leiakc.github.io/portfolio/between_the_cards.html";
- });
-
- var summaryOSLPLink = document.getElementById("summaryOSLPLink");
-
- summaryOSLPLink.addEventListener("click", function() {
-     // Navigate to the target URL when clicked
-     window.location.href = "https:/leiakc.github.io/portfolio/summary_olsp.html";
- });
-
- var socialOrbitLink = document.getElementById("socialOrbitLink");
-
- socialOrbitLink.addEventListener("click", function() {
-     // Navigate to the target URL when clicked
-     window.location.href = "https:/leiakc.github.io/portfolio/social_orbit.html";
- });
-
- var acguLink = document.getElementById("acguLink");
-
- acguLink.addEventListener("click", function() {
-     // Navigate to the target URL when clicked
-     window.location.href = "https:/leiakc.github.io/portfolio/children_confiding_in_parents.html";
- });
-
- var veraxLink = document.getElementById("veraxLink");
-
- veraxLink.addEventListener("click", function() {
-     // Navigate to the target URL when clicked
-     window.location.href = "https:/leiakc.github.io/portfolio/verax.html";
- });
